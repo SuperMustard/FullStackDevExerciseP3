221157397c9aca222ac55a011846d0975419d620
SuperMustard <mr.jiahanxin@gmail.com>
2023-07-13 16:51:30 +0800

    add lint

--- index.js
+++ index.js
@@ -9,34 +9,11 @@ app.use(cors())
 const morgan = require('morgan')
 app.use(morgan('tiny'))
 app.use(express.json())
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   next()
-});
+})
 
 const Person = require('./models/person')
-
-// let persons = [
-//     { 
-//       "id": 1,
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": 2,
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": 3,
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": 4,
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ]
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -46,92 +23,91 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/api/info', (request, response) => {
   Person.find({}).then(persons => {
-    let dt = new Date();
-    let fisrLine = `<h2> Phonebook has info for ${persons.length} people </h2>`
-    let secondLine = `<p>${dt}</p>`
+    const dt = new Date()
+    const fisrLine = `<h2> Phonebook has info for ${persons.length} people </h2>`
+    const secondLine = `<p>${dt}</p>`
     response.send(fisrLine + secondLine)
   })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
     .catch(error => next(error))
-  })
+})
 
-  app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-    
-    console.log(person);
-
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
-      .then(updatedPerson => {
-        response.json(updatedPerson)
-      })
-      .catch(error => next(error))
-  })
-
-  app.post('/api/persons', (request, response, next) => {
-    const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({ 
-        error: 'name missing' 
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({ 
-        error: 'number missing' 
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
-
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
-  })
-
-  const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-      console.log(error.message)
-      return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
+  const person = {
+    name: body.name,
+    number: body.number
   }
-  
-  // this has to be the last loaded middleware.
-  app.use(errorHandler)
+  console.log(person)
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    console.log(error.message)
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
-    app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
